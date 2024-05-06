54d25582fd54c6628219ff308981214cdf624697
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-05-06 15:39:05 +0200

    [MOD] - / to /laravel-relaciones

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,7 @@ import HasOneOfMany from "../components/HasOneOfMany"
 import ManyToMany from "../components/ManyToMany"
 
 export const routes = [
-    { path: '/', name: 'Home', element: createElement(Home) },
+    { path: '/laravel-relaciones', name: 'Home', element: createElement(Home) },
     { path: '/one-to-one', name: 'OneToOne', element: createElement(OneToOne) },
     { path: '/one-to-many', name: 'OneToMany', element: createElement(OneToMany) },
     { path: '/has-one-of-many', name: 'HasOneOfMany', element: createElement(HasOneOfMany) },
